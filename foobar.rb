89147595cfc8efb176cded4d64db8569e4ba807c
christopherquan <chquan@berkeley.edu>
2016-09-21 11:16:34 -0700

    finish hw1

--- foobar.rb
+++ foobar.rb
@@ -1,8 +1,7 @@
 class Foobar
 
   def self.baz(a)
-    # Class method
-    # Call with `Foobar.baz`
+    a.map{ |s| s.to_i + 2}.uniq.select{ |i| i <= 10 && i.even?}.inject(0){|sum,x| sum + x }
   end
 end
 
